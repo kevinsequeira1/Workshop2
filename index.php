9430fc6136cbebb02ad98494c42a8648506f0986
kevinsequeira1 <kevinsequeira23@gmail.com>
2020-01-28 10:10:11 -0600

    Workshop finalizado

--- index.php
+++ index.php
@@ -29,13 +29,31 @@
     <div>
         <h3>Temperaturas</h3>
         <?php
-        $list = array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
-        foreach ($list as $n){
-            echo "$n,";
-            $n++;
-        }
-        
+            $month_temp = "78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 81, 76, 73,
+            68, 72, 73, 75, 65, 74, 63, 67, 65, 64, 68, 73, 75, 79, 73";
+            $temp_array = explode(',', $month_temp);
+            $tot_temp = 0;
+            $temp_array_length = count($temp_array);
+            foreach($temp_array as $temp)
+            {
+            $tot_temp += $temp;
+            }
+            $avg_high_temp = $tot_temp/$temp_array_length;
+            echo " Temperatura promedio es : ".$avg_high_temp."
+            "; 
+            sort($temp_array);
+            echo " Las 5 temperaturas mas bajas :";
+            for ($i=0; $i< 5; $i++)
+            { 
+            echo $temp_array[$i].", ";
+            }
+            echo "Las 5 temperaturas mas altas :";
+            for ($i=($temp_array_length-5); $i< ($temp_array_length); $i++)
+            {
+            echo $temp_array[$i].", ";
+            }
         ?>
+
     </div>
 
 </body>
